ed2a9e68f7f46cf77de2b6a6246cf8555f44d747
Pranay Patil <itachiuchiha9604@gmail.com>
2022-06-23 00:41:38 +0530

    added collapse-expand functionality

--- src/components/BaseObject.js
+++ src/components/BaseObject.js
@@ -1,10 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../App.css';
 
 function randomColor(){
-    setInterval(() => {
-        
-    }, 100);
     let colors = ["red", "purple", "blue", "#442200", "orange","#0f3b57", "#11574a", "#985538", "#674c47"]
     let fontColor = colors[Math.floor(Math.random() * colors.length)];
     return ({ color: fontColor });
@@ -13,9 +10,22 @@ function randomColor(){
 
 export default function BaseObject(props) {
     let s = randomColor();
+    const [exact_value, setexact_value] = useState(props.data)
+    let toggleData = ()=>{
+        if(exact_value === props.data){
+            setexact_value("...")
+            console.log("changed to ...")
+        }
+        else{
+            setexact_value(props.data)
+            console.log("changed to data")
+        }
+    }
+
+
     return (
-        <div className="base-component">
-            <span className="section-name">.{props.name}</span><span style={s} className="curly-braces">{"{"}</span><span className={props.cssProperty}>{props.data}</span><span style={s} className="level-one-margin curly-braces">{"}"}</span><span className="end-brackets"> \\ {props.name}</span>
+        <div className="base-component" >
+            <span className="section-name" onClick={toggleData}>.{props.name}</span><span style={s} className="curly-braces">{"{"}</span><span className={props.cssProperty}>{exact_value}</span><span style={s} className="level-one-margin curly-braces">{"}"}</span><span className="end-brackets"> \\ {props.name}</span>
         </div>
     )
 }
